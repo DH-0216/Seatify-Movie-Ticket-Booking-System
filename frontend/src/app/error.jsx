0c8f349c0cba401eda296e17b17150da70e7574e
Dulaj Hashmika <dulajhashmika075@gmail.com>
2025-07-15 01:09:10 +0530

    update hero section

---
+++ frontend/src/app/error.jsx
@@ -0,0 +1,11 @@
+"use client";
+const error = () => {
+  return (  
+    <div>
+      <h1 className="text-4xl font-bold">An error occurred</h1>
+    </div>
+  )
+}
+
+export default error
+    
--- frontend/src/components/sections/HeroSection/page.jsx
+++ frontend/src/components/sections/HeroSection/page.jsx
@@ -1,9 +1,12 @@
+"use client";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
-import { Calendar1Icon, ClockIcon } from "lucide-react";
+import { ArrowRight, Calendar1Icon, ClockIcon } from "lucide-react";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const Hero = () => {
+  const router = useRouter();
   return (
     <div className="flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-hero bg-cover bg-center h-screen">
       <Image
@@ -25,6 +28,18 @@ const Hero = () => {
           <ClockIcon className="w-4.5 h-4.5" /> 2h 8m
         </div>
       </div>
+      <p className="max-w-md text-gray-300">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, amet
+        veniam, incidunt porro eveniet facilis praesentium illo officia quam ea,
+        alias enim ullam voluptatem? Ea corrupti eos laborum iure autem.
+      </p>
+      <button
+        onClick={() => router.push("/movies")}
+        className="flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+      >
+        Explore Movies
+        <ArrowRight className="w-5 h-5" />
+      </button>
     </div>
   );
 };
