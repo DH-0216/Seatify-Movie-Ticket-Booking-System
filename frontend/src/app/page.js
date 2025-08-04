0574c1ccb1383faf0a04f95a735c4ee5147cb4bd
Dulaj Hashmika <dulajhashmika075@gmail.com>
2025-08-04 13:39:51 +0530

    update

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,14 @@
+import HeroSection from "@/components/sections/HomeSections/HeroSection";
 import HomePage from "./HomePage/page";
+import FeaturedSection from "@/components/sections/HomeSections/FeaturedSection";
+import TrailersSection from "@/components/sections/HomeSections/TrailersSection";
 
 export default function Home() {
   return (
     <>
-      <HomePage/>
+      <HeroSection />
+      <FeaturedSection />
+      <TrailersSection/>
     </>
   );
 }
