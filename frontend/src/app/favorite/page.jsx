6bcc8f309e9c2962cc9fe2379e11e1250257e49e
Thisari Hettiarachchi <msthisari@gmail.com>
2025-08-05 12:14:14 +0530

    movies, favorite

--- frontend/src/app/favorite/page.jsx
+++ frontend/src/app/favorite/page.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { dummyShowsData } from '@/data/index'
+import MovieCard from '@/components/MovieCard'
+import BlurCircle from '@/components/shared/BlurCircle'
 
 const Favorite = () => {
-  return (
-    <div>
-      
+  return dummyShowsData.length > 0 ? (
+    <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hiddenmin-h-[80vh]'>
+
+      <BlurCircle top='150px' left='0px'/>
+      <BlurCircle bottom='50px' right='50px'/>
+      <h1 className='txt-lg font-medium my-4'>Your Favorie Movies</h1>
+      <div className='flex flex-wrap max-sm:justify-center gap-8'>
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie._id} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-3xl font-bold text-center'>No movies available</h1>
     </div>
   )
 }
--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,3 +1,4 @@
+"use client"
 import timeFormat from "@/lib/timeFormat";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
