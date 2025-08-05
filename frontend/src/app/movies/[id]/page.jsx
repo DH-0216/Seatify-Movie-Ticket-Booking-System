687f41010cb81bc32c25dee796a24157079e87cc
Thisari Hettiarachchi <msthisari@gmail.com>
2025-08-05 23:44:04 +0530

    fix

--- frontend/src/app/movies/[id]/page.jsx
+++ frontend/src/app/movies/[id]/page.jsx
@@ -1,16 +1,22 @@
-import DateSelect from '@/components/DateSelect';
-import FavoriteCast from '@/components/sections/MovieSections/FavoriteCast';
-import MoreDetails from '@/components/sections/MovieSections/MoreDetails';
-import React from 'react'
+"use client";
+import DateSelect from "@/components/DateSelect";
+import FavoriteCast from "@/components/sections/MovieSections/FavoriteCast";
+import MoreDetails from "@/components/sections/MovieSections/MoreDetails";
+import React from "react";
+import { dummyDateTimeData, dummyShowsData } from "@/data/index";
+import { useParams } from "next/navigation";
 
 const MovieDetails = () => {
+  const { id } = useParams();
+  const movie = dummyShowsData.find((movie) => movie._id === id);
+
   return (
     <div>
       <MoreDetails />
       <FavoriteCast />
-      <DateSelect />
+      <DateSelect dateTime={movie?.dateTime || {}} id={movie?._id} />
     </div>
-  ) 
+  );
 };
 
-export default MovieDetails
+export default MovieDetails;
