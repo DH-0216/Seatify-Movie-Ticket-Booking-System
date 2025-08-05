ebd259cf72b3e96dbbd486c4f99309548a207642
Dulaj Hashmika <dulajhashmika075@gmail.com>
2025-08-05 21:26:02 +0530

    update admin logo

--- admin/src/components/Shared/AdminNavBar.jsx
+++ admin/src/components/Shared/AdminNavBar.jsx
@@ -1,19 +1,13 @@
 "use client";
 
-
-
+import { assets } from "@/assets/assets";
+import Image from "next/image";
 import React from "react";
-import Logo from "../Logo";
-
 
 const AdminNavBar = () => {
-
   return (
     <div className="flex items-center justify-between px-6 md:px-10 h-16 border-b border-gray-300/30">
-      <Logo />
-
-
-
+      <Image src={assets.logo} alt="" className="w-36 h-auto" />
     </div>
   );
 };
