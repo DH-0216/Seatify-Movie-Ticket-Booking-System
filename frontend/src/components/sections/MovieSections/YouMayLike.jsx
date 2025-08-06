c70114a146d2d14ddd3538091a5fc5f0993d6cfe
Thisari Hettiarachchi <msthisari@gmail.com>
2025-08-06 12:04:56 +0530

    update

--- frontend/src/components/sections/MovieSections/YouMayLike.jsx
+++ frontend/src/components/sections/MovieSections/YouMayLike.jsx
@@ -8,8 +8,8 @@ const YouMayLike = () => {
   const router = useRouter();
 
   return (
-    <div className="px-6 md:px-16 lg:px-40 pt-8 md:pt-10">
-      <p className="text-lg font-medium mb-8 mt-20">You May Also Like</p>
+    <div className="px-6 md:px-16 lg:px-40 pt-5 md:pt-10">
+      <p className="text-lg font-medium mb-8 mt-10">You May Also Like</p>
 
       <div className="flex flex-wrap max-sm:justify-center gap-8">
         {dummyShowsData.slice(0, 6).map((movie, index) => (
