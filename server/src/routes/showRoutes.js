96487fdac8178280bffe1be2387d111839352106
Thisari Hettiarachchi <msthisari@gmail.com>
2025-09-20 16:17:20 +0530

    Update showRoutes.js

--- server/src/routes/showRoutes.js
+++ server/src/routes/showRoutes.js
@@ -5,7 +5,9 @@ import { addShow } from '../controllers/showController.js';
 
 const showRouter = express.Router();
 
-showRouter.get('/now-playing', getNowPlayingMovies)
-showRouter.post('/add', addShow)
+showRouter.get('/now-playing', protectAdmin, getNowPlayingMovies)
+showRouter.post('/add', protectAdmin, addShow)
+showRouter.get("/all", getShows)
+showRouter.get("/:movie_id", getShow)
 
 export default showRouter;
