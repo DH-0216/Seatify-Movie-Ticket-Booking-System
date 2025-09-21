4829b86dd869a201fd3f874d9fc5c202a497866b
BiyangaKalupahana <biyangadilmini1@gmail.com>
2025-09-21 12:22:30 +0530

    update bookingsection
    from "to" to "href"

--- client/src/components/sections/MyBookingsSections/BookingsSection.jsx
+++ client/src/components/sections/MyBookingsSections/BookingsSection.jsx
@@ -59,7 +59,7 @@ const MyBookingsSection = () => {
                                 {currency}{item.amount}
                             </p>
                             {!item.isPaid && (
-                                <Link to={item.paymentLink} className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>
+                                <Link href={item.paymentLink} className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>
                                     Pay Now
                                 </Link>
                             )}
