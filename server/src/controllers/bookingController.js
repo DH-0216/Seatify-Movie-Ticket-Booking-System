408db67b4e29d7eb109f042c74d129f85ec09337
BiyangaKalupahana <biyangadilmini1@gmail.com>
2025-09-22 01:04:39 +0530

    check payment status

--- server/src/controllers/bookingController.js
+++ server/src/controllers/bookingController.js
@@ -1,6 +1,7 @@
 import Show from "../models/Show.js";
 import Booking from "../models/Booking.js";
 import stripe from 'stripe';
+import { inngest } from "../inngest/index.js";
 
 
 //function to check availability of selected seats for a movie
@@ -78,6 +79,14 @@ export const createBooking = async (req, res) => {
 
         booking.paymentLink = session.url
         await booking.save()
+
+        //run inngest scheduler function to check payment status after 10 minutes
+        await inngest.send({
+	        name: "app/checkpayment",
+	        data: {
+		        bookingId: booking._id.toString()
+	        }
+        })
 
         //stripe gateway initialize
         res.json({success: true, url: session.url})
